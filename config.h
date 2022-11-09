b0c8d8255205b9b378bf0d6ecfaa8391969a9a9c
reubenvisser <reubenvisser@outlook.com>
2022-11-09 18:41:11 +1000

    fixed defines

--- config.h
+++ config.h
@@ -1,3 +1,5 @@
-#define NUM_ENTRANCES = 5
-#define NUM_EXITS = 5
-#define NUM_LEVELS = 5
+#define NUM_ENTRANCES 5
+#define NUM_EXITS 5
+#define NUM_LEVELS 5
+
+#define SHM_SIZE 2920
---
+++ simulator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "config.h"
+#include "sharedMemory.h"
+
+int main(int argc, char **argv) {
+
+    return 0;
+}
